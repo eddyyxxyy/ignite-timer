3f5141b120f9a65bb30b7fabb84c9440d9799fb4
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-11-06 09:47:38 -0300

    feat(routes): :sparkles: creates and implements basic route system

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
+import { Router } from './Router';
 import { GlobalStyles } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
@@ -7,6 +9,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
