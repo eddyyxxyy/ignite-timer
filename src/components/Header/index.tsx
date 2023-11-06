2cd7cc3b8d435d7a74599b21501464380d56033c
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-11-06 10:33:29 -0300

    feat(components): :sparkles: creates and styles the Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { Scroll, Timer } from 'phosphor-react';
+import { NavLink } from 'react-router-dom';
+
+import logoIgnite from '../../assets/logo-ignite.svg';
+import { HeaderContainer } from './styles';
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+}
